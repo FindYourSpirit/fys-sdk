ff4fca4fe4b63c92b6385f9ba44af6c20cab78d9
Timothy Los <lostimothy1985@gmail.com>
2023-06-02 09:07:59 -0700

    src and tools class files for FYS SDK API

---
+++ src/SdkClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FYS_SDK.src
+{
+    internal class SdkClient
+    {
+    }
+}
