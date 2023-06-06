8c05b4f1312a4391cec1f504ccc1fa67e995b663
Timothy Los <lostimothy1985@gmail.com>
2023-06-06 11:25:10 -0700

    Added basic ApiResponse.cs

--- src/ApiResponse.cs
+++ src/ApiResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,49 @@ using System.Threading.Tasks;
 
 namespace FYS_SDK.src
 {
-    public class ApiResponse
+    public class ApiResponse<T>
     {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+
+        public ApiResponse()
+        {
+            // Default constructor
+        }
+
+        public ApiResponse(int statusCode, string message, T data)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Data = data;
+        }
+
+        public bool IsSuccess()
+        {
+            return StatusCode >= 200 && StatusCode < 300;
+        }
+
+        public bool HasData()
+        {
+            return Data != null;
+        }
+
+        public T DeserializeData()
+        {
+            // Implement deserialization logic here
+            // For example, using Newtonsoft.Json:
+            return JsonConvert.DeserializeObject<T>(Data.ToString());
+        }
+
+        public static ApiResponse<T> FromError(string errorMessage)
+        {
+            return new ApiResponse<T>
+            {
+                StatusCode = 500,
+                Message = errorMessage
+            };
+        }
     }
+
 }
